2dd057b1225eab6c034a3448b6bffff59e4ec70b
Iain-S <25081046+Iain-S@users.noreply.github.com>
2021-11-15 17:28:53 +0000

    Add first unit tests for main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-"""Generate different editions of the book, as determined by profiles.yml"""
+"""Generate different editions of the book, as determined by profiles.yml."""
 
 
 def main():
     pass
+
+
+def mask_dict(toc_dict, profiles_dict):
+    """For each profile, generate a masked _toc dict."""
+
+    return toc_dict
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,30 +3,46 @@ import unittest
 from io import StringIO
 from unittest import mock
 
-import main
+from yaml import Loader, load
+
+from main import main, mask_dict
 
 
 class MyTestCase(unittest.TestCase):
-    """Test functions from the hello module."""
+    """Test functions from the main module."""
 
     def test_get_message(self):
-        actual = main.main()
+        actual = main()
         self.assertIsNone(actual)
 
     def test_main(self):
         # Redirect stdout
         with mock.patch("sys.stdout", new=StringIO()) as mock_out:
-            main.main()
+            main()
 
         actual = mock_out.getvalue().strip().split("\n")
         expected = [""]
         self.assertListEqual(expected, actual)
 
-    @unittest.skip
-    def test_new_function(self):
-        actual = main.main()
-        expected = 32
-        self.assertEqual(expected, actual)
+
+class TestMask(unittest.TestCase):
+    """Test the mask function from the main module."""
+
+    def test_mask_empty(self):
+        mask_dict({}, {})
+
+    def test_mask_single(self):
+        profiles = {"dsg": ["intro", "setup", "version_control/git"]}
+
+        with open("tests/test_files/test_one/_toc.yml", "r") as f:
+            toc = load(f, Loader=Loader)["parts"]
+
+        with open("tests/test_files/test_one/dsg_toc.yml", "r") as f:
+            expected = load(f, Loader=Loader)["parts"]
+
+        actual = mask_dict(toc, profiles)
+
+        self.assertListEqual(expected, actual)
 
 
 if __name__ == "__main__":
